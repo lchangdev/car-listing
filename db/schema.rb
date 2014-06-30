0d92cb26815c7a3b407074817c728aec36c11f9f
Lawrence Chang <lc.uclakers@gmail.com>
2014-06-29 21:27:30 -0400

    create cars table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140630004050) do
+ActiveRecord::Schema.define(version: 20140630012314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cars", force: true do |t|
+    t.integer "manufacturer_id", null: false
+    t.string  "color",           null: false
+    t.integer "year",            null: false
+    t.integer "mileage",         null: false
+    t.text    "description"
+  end
 
   create_table "manufacturers", force: true do |t|
     t.string   "name",       null: false
