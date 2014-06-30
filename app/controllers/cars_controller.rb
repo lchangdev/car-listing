10df9fa05d66d42ebcd7f2547ffe7cb43899938c
Lawrence Chang <lc.uclakers@gmail.com>
2014-06-29 21:42:50 -0400

    included create ane new methods for car, created car new page, created routes for cars

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,3 +1,23 @@
 class CarsController < ApplicationController
 
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(car_params)
+    if @car.save
+      flash[:notice] = "Success"
+      redirect_to cars_path
+    else
+      flash[:notice] = "Error"
+      render :new
+    end
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(:manufacturer, :color, :year, :mileage, :description)
+  end
 end
