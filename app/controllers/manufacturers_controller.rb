906548e2163195bbd6a78acacf81515440c051a9
Lawrence Chang <lc.uclakers@gmail.com>
2014-06-29 20:49:50 -0400

    created new, create, manufacturer_params methods in manufacturers controller

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -1,3 +1,23 @@
 class ManufacturersController < ApplicationController
 
+  def new
+    @manufacturer = Manufacturer.new
+  end
+
+  def create
+    @manufacturer = Manufacturer.new(manufacturer_params)
+
+    if @manufacturer.save
+      flash[:notice] = "Success"
+      redirect_to manufacturers_path
+    else
+      flash[:notice] = "Error"
+      render :new
+  end
+
+  private
+
+  def manufacturer_params
+
+  end
 end
